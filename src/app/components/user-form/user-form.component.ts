d34ac13e3f82d3a7f17883aba3cb9c145a3bf9bf
gehringerhugo <hugo.gehringer@free.fr>
2024-10-11 12:48:26 +0200

    docs

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,27 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {UserService} from '../../services/user-service.service';
-import {
-  AbstractControl,
-  FormControl,
-  FormGroup,
-  ValidationErrors,
-  ValidatorFn,
-  Validators
-} from '@angular/forms';
-import {ToastrService} from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user-service.service';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
+/**
+ * Component for the user form.
+ */
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
-export class UserFormComponent implements  OnInit{
+export class UserFormComponent implements OnInit {
   userForm!: FormGroup;
 
+  /**
+   * Constructor for UserFormComponent.
+   * @param {UserService} userService - The user service.
+   * @param {ToastrService} toastr - The toastr service for notifications.
+   */
+  constructor(private userService: UserService, private toastr: ToastrService) {}
 
-  constructor(private userService: UserService, private toastr: ToastrService) {
-  }
-
+  /**
+   * Initializes the component and sets up the form.
+   */
   ngOnInit(): void {
     this.userForm = new FormGroup({
       'firstName': new FormControl(null, Validators.required),
@@ -29,19 +31,27 @@ export class UserFormComponent implements  OnInit{
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'birthDate': new FormControl(null, [Validators.required, this.customAgeValidator()]),
       'city': new FormControl(null, Validators.required),
-      'postalCode': new FormControl(null,[ Validators.required, this.customPostalCodeValidator()])
+      'postalCode': new FormControl(null, [Validators.required, this.customPostalCodeValidator()])
     });
   }
 
-  customAgeValidator():ValidatorFn {
-    return  (control: AbstractControl):ValidationErrors|null => {
+  /**
+   * Custom validator to check if the user's age is below 18.
+   * @returns {ValidatorFn} The validator function.
+   */
+  customAgeValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
       if (!control.value) return null;
       const age = this.userService.calculateAge(new Date(control.value));
       console.debug(age);
-      return age < 18 ? {ageBelow18: true} : null;
-    }
+      return age < 18 ? { ageBelow18: true } : null;
+    };
   }
 
+  /**
+   * Custom validator to check if the postal code is valid.
+   * @returns {ValidatorFn} The validator function.
+   */
   customPostalCodeValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const value = control.value;
@@ -50,6 +60,9 @@ export class UserFormComponent implements  OnInit{
     };
   }
 
+  /**
+   * Handles the form submission.
+   */
   onSubmit() {
     if (this.userForm.invalid) {
       this.userForm.markAllAsTouched();
@@ -57,7 +70,5 @@ export class UserFormComponent implements  OnInit{
     }
     this.userForm.reset();
     this.toastr.success('Utilisateur ajouté avec succès', 'Succès');
-    }
+  }
 }
-
-
--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,10 +1,17 @@
 import {Injectable} from '@angular/core';
-
+/**
+ * Service to handle user-related operations.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  /**
+   * Calculates the age based on the birthdate.
+   * @param {Date} birthDate - The birthdate of the user.
+   * @returns {number} The calculated age.
+   * @throws Will throw an error if the birthDate is invalid.
+   */
   calculateAge(p: Date) {
     if (isNaN(p.getTime())) {
       throw new Error('Invalid date');

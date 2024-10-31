0fe0614bcef1e6c0ca5cea354d80fc1411add01e
gehringerhugo <hugo.gehringer@free.fr>
2024-10-31 14:44:46 +0100

    test npm publish

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export default class AppComponent {
+export class AppComponent {
   title = 'ynov-deploiement-formulaire-angular';
 }

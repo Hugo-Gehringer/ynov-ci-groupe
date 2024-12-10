157dc5f4ea862f3c1cf9f0898b367060c41b6ded
gehringerhugo <hugo.gehringer@free.fr>
2024-12-10 10:46:59 +0100

    remove comment

--- front/cypress/e2e/admin-login.cy.ts
+++ front/cypress/e2e/admin-login.cy.ts
@@ -1,16 +1,11 @@
 describe('Admin Login', () => {
   it('should display an error message for incorrect credentials', () => {
-    // Visite la page de connexion
     cy.visit('http://localhost:4200/login');
 
-    // Saisit des identifiants incorrects
     cy.get('input[id="email"]').type('admin@admin.com');
     cy.get('input[id="password"]').type('wrongpassword');
 
-    // Soumet le formulaire de connexion
     cy.get('button[type="submit"]').click();
-
-    // Vérifie que le message d'erreur est affiché
 
     cy.get('#toast-container')
       .should('be.visible')

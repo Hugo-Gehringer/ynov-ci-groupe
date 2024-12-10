b406b69453cec23408c2b4aa79de6fd6ce0e325b
Maxime PALLEJA <maxime.palleja@gmail.com>
2024-12-10 09:45:06 +0100

    ajout test

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -1,6 +1,12 @@
-describe('Home page spec', () => {
+describe('Login page spec', () => {
   it('deployed react app to localhost', () => {
     cy.visit('http://localhost:4200/login')
+    cy.contains("Se connecter")
+  })
+})
+describe('Register page spec', () => {
+  it('deployed react app to localhost', () => {
+    cy.visit('http://localhost:4200/register')
     cy.contains("S'inscrire")
   })
 })

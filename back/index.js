77597a9a68954c38d24bb4b2c9f0269a13c4d400
gehringerhugo <hugo.gehringer@free.fr>
2024-12-10 11:47:40 +0100

    db mongo atlas uri

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-const dbUri = "mongodb+srv://gehringerhugo:czdsi388bFsr81DT@cluster0.0v0lc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbUri = "mongodb+srv://gehringerhugo:czdsi388bFsr81DT@cluster0.0v0lc.mongodb.net/project_db?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(dbUri, {
     useNewUrlParser: true,

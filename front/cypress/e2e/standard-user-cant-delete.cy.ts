c4b12a709d8060da2cc8b4db7bc917e8b4e203f9
Maxime PALLEJA <maxime.palleja@gmail.com>
2024-12-10 10:48:07 +0100

    modif url

--- front/cypress/e2e/standard-user-cant-delete.cy.ts
+++ front/cypress/e2e/standard-user-cant-delete.cy.ts
@@ -3,7 +3,7 @@
 describe('Standard User Cannot See Delete Button', () => {
   it('should not display the delete button for a non-logged-in user', () => {
     // Visit the users page
-    cy.visit('http://localhost:4200/users');
+    cy.visit('http://localhost:4200/users-list');
 
     // Verify that the delete button is not visible
     cy.get('button.delete-user').should('not.exist');
